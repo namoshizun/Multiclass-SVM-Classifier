ff7d518609ec6280f60c9dcce42bb4bc80819c38
namoshizun <dilu3100@gmail.com>
2017-05-05 22:53:27 +1000

    experiment results for feature selection and 10 fold cv for the top 750 most informative features. improve performance of prediciton functions

--- algorithm/main.py
+++ algorithm/main.py
@@ -9,6 +9,7 @@ def read_small_data():
     return pd.read_csv(source, index_col=0), None
 
 
+
 def read_full_data():
     training_data = '../input/training_data.csv'
     training_labels = '../input/training_labels.csv'
@@ -16,34 +17,23 @@ def read_full_data():
 
     training_data = make_training_data(training_data, training_labels)
     test = build_dataframe(test_data)
-    feature_selection(training_data)
+    feature_selection(training_data, use_cache=True)
 
     return training_data, test
 
-
-def read_mock_data():
-    training_data = '../input-dev/mock_training.csv'
-    training_labels = '../input-dev/mock_training_labels.csv'
-    test_data = '../input-dev/mock_test.csv'
-    test_labels = '../input-dev/mock_test_label.csv'
-    training_data = make_training_data(training_data, training_labels)
-    test = build_dataframe(test_data)
-
-    return training_data, test
 
 if __name__ == '__main__':
     # RECEIVE CONFIG
     parser = argparse.ArgumentParser(description='SVM Classifier')
     parser.add_argument('kernel', nargs='?', type=str, default='linear', help='The kernel function to use')
-    parser.add_argument('strategy', nargs='?', type=str, default='one_vs_one', help='The strategy to implement a multiclass SVM. Choose "one_vs_one" or "one_vs_rest"')
+    parser.add_argument('strategy', nargs='?', type=str, default='one_vs_rest', help='The strategy to implement a multiclass SVM. Choose "one_vs_one" or "one_vs_rest"')
     parser.add_argument('C', nargs='?', type=float, default=1.0, help='The penalty constant C value')
     parser.add_argument('min_lagmult', nargs='?', type=float, default=1e-5,
                         help='The support vector\'s minimum lagrange multipliers value')
     config = vars(parser.parse_args())
 
-    # training_data, test = read_mock_data()
-    training_data, test = read_full_data()
-    # training_data, test = read_small_data()
+    # training_data, test = read_full_data()
+    training_data, test = read_small_data()
 
     # HAVE FUN!
     trainer = Trainer(training_data, config)
--- algorithm/util.py
+++ algorithm/util.py
@@ -4,6 +4,8 @@ import numpy as np
 import os, time, shutil
 from collections import Counter, defaultdict
 from numba import jit
+from prettytable import PrettyTable
+
 
 ##################
 # MISC Utilities #
@@ -70,13 +72,83 @@ def poly(degree=3, offset=1e0):
 #################################
 # Data pre-processing utilities #
 #################################
-def read_selected_features(path):
+def build_dataframe(source):
+    # read source data, use the app name as index
+    print('===== building data frame for {} ====='.format(os.path.basename(source)))
+
+    data = pd.read_csv(source, index_col=0, header=None)
+    data.index = data.index.rename('app')
+
+    # create semantic columns
+    tagged = ['t' + str(i) for i in range(len(data.columns))]
+    data.columns = tagged
+
+    return data
+
+
+def make_training_data(training_path, labels_path):
+    # read training and label data, use app name as index
+    data = build_dataframe(training_path)
+    print('===== making training data =====')
+
+    # read labels and join them together
+    labels = pd.read_csv(labels_path, index_col=0, header=None)
+    labels.index = labels.index.rename('app')
+    labels.columns = ['labels']
+    assert(len(data) == len(labels))
+
+    # nice and clean
+    data = data.join(labels)
+    data.set_index(['labels'], inplace=True)
+
+    return data
+
+
+def feature_selection(training_dataframe, min_ig=0.0107036, use_cache=False):
+    """
+    Compute information gain to remove redundant features.
+    Reference: [A survey of text classification algorithms](www.time.mk/trajkovski/thesis/text-class.pdf)
+    """
+    print('===== selecting usefull features [usecache={}] ====='.format(use_cache))
+    features = training_dataframe.columns
+    if use_cache:
+        features_ig = read_info_gain()
+    else:
+        features_ig = compute_info_gains(training_dataframe, save=True)
+
+    selected_features = [ft for ft, ig in features_ig.items() if ig >= min_ig]
+    redundant_features = list(set(features) - set(selected_features))
+    training_dataframe.drop(redundant_features, axis=1, inplace=True)
+
+
+def read_info_gain(path='./information_gain_result.txt'):
+    import re
     with open(path, 'r') as source:
-        return source.read().splitlines()
+        features_ig = {}
+        regex=re.compile(r'^|\s+\w+\s+|\s+\d+\.\d+\s+|$')
+        lines = source.read().splitlines()[3:]
+        for ln in lines:
+            parts = regex.findall(ln)
+            if len(parts) != 4: continue
+            features_ig[parts[1].strip()] = float(parts[2].strip())
+
+        return features_ig
+
+
+def save_info_gain(result, path='./information_gain_result.txt'):
+    from operator import itemgetter
+    pt = PrettyTable()
+    pt.add_column('feature name', list(result.keys()))
+    pt.add_column('information gain', list(result.values()))
+    pt.reversesort = True
+    pt.sortby = 'information gain'
+
+    with open(path, 'w+') as thefile:
+        thefile.write(pt.get_string())
 
 
 @timing
-def compute_info_gains(training_dataframe):
+def compute_info_gains(training_dataframe, save=False):
     def entropy(vals):
         if len(vals) == 0:
             return 0.0
@@ -99,6 +171,7 @@ def compute_info_gains(training_dataframe):
     N = len(training_dataframe)
     word_ig = {}
 
+    print('*** computing info gain ***')
     for word in features:
         selector = training_dataframe[word] > 0
         w_docs =  [i for i, ok in selector.iteritems() if ok]
@@ -106,54 +179,6 @@ def compute_info_gains(training_dataframe):
         F_w = selector.sum() / N
         word_ig[word] = kls_entropy - F_w * entropy(w_docs) - (1 - F_w) * entropy(nw_docs)
 
+    if save:
+        save_info_gain(word_ig)
     return word_ig
-
-
-def feature_selection(training_dataframe, min_ig=0.015, use_cached=False):
-    """
-    Compute information gain to remove redundant features.
-    Reference: [A survey of text classification algorithms](www.time.mk/trajkovski/thesis/text-class.pdf)
-    """
-    # # remove unused features
-    features = training_dataframe.columns
-    if use_cached:
-        selected_features = read_selected_features('./selected_features.txt')  # TODO
-    else:
-        features_ig = compute_info_gains(training_dataframe)
-        selected_features = [ft for ft, ig in features_ig.items() if ig >= min_ig]
-
-    redundant_features = list(set(features) - set(selected_features))
-    training_dataframe.drop(redundant_features, axis=1, inplace=True)
-
-
-
-def build_dataframe(source):
-    # read source data, use the app name as index
-    print('===== building data frame for {} ====='.format(os.path.basename(source)))
-
-    data = pd.read_csv(source, index_col=0, header=None)
-    data.index = data.index.rename('app')
-
-    # create semantic columns
-    tagged = ['t' + str(i) for i in range(len(data.columns))]
-    data.columns = tagged
-
-    return data
-
-
-def make_training_data(training_path, labels_path):
-    # read training and label data, use app name as index
-    data = build_dataframe(training_path)
-
-    # read labels and join them together
-    labels = pd.read_csv(labels_path, index_col=0, header=None)
-    labels.index = labels.index.rename('app')
-    labels.columns = ['labels']
-    assert(len(data) == len(labels))
-
-    # nice and clean
-    data = data.join(labels)
-    data.set_index(['labels'], inplace=True)
-
-    return data
-
